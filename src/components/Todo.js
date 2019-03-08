b579872b54cc8a6c32c7fdb233a4cf8b1a7383c6
Valery Filipyev <valeranuzhdin1998@mail.ru>
2019-03-08 17:13:36 +0300

    how to use useEffect

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,5 @@
 //Hooks use only in the root of the code without any nesting (for, if and so on)
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const todo = props => {
@@ -7,6 +7,24 @@ const todo = props => {
   const [todoList, setTodoList] = useState([]);
   
   // const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
+  
+  useEffect(() => {
+    axios.get('https://test-326e3.firebaseio.com/todos.json')
+      .then(result => {
+        console.log(result);
+        
+        const todoData = result.data;
+        const todos = [];
+        for (const key in todoData) {
+          if (todoData.hasOwnProperty(key)) {
+            todos.push({
+              id: key,
+              name: todoData[key].name})
+          }
+        }
+        setTodoList(todos);
+      });
+  });
   
   const inputChangeHandler = event => {
     // setTodoState({
@@ -45,7 +63,7 @@ const todo = props => {
       </button>
       <ul>
         {todoList.map(todo => (
-          <li key={todo}>{todo}</li>
+          <li key={todo.id}>{todo.name}</li>
         ))}
       </ul>
     </>
