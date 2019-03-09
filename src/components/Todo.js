8061ecaaa523964583a3a0d94ddf7a0edc99b465
Valery Filipyev <valeranuzhdin1998@mail.ru>
2019-03-09 13:30:58 +0300

    how to use useEffect()
    if we use return and pass the second argument empty array or smth like this, we will see CDUm and CDM

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -24,6 +24,20 @@ const todo = props => {
         }
         setTodoList(todos);
       });
+    return () => {
+      console.log('Cleanup');
+    }
+  }, []);
+  
+  const mouseMoveHandler = event => {
+    console.log(event.clientX, event.clientY);
+  };
+  
+  useEffect(() => {
+    document.addEventListener('mousemove', mouseMoveHandler);
+    return () => {
+      document.removeEventListener('mousemove', mouseMoveHandler)
+    }
   }, []);
   
   const inputChangeHandler = event => {
