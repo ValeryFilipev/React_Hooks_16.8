84f6236481eb1a636aa5327eec23f1395285b387
Valery Filipyev <valeranuzhdin1998@mail.ru>
2019-03-17 21:20:00 +0300

    how to use useRef()!
    The cool hook for some cases, the code becomes shorter.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,13 @@
 //Hooks use only in the root of the code without any nesting (for, if and so on)
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useRef } from 'react';
 import axios from 'axios';
 
 const todo = props => {
-  const [todoName, setTodoName] = useState('');
+  // const [todoName, setTodoName] = useState('');
   // const [submittedTodo, setSubmittedTodo] = useState(null);
   // const [todoList, setTodoList] = useState([]);
-  
   // const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
+  const todoInputRef = useRef();
   
   const todoListReducer = (state, action) => {
     switch (action.type) {
@@ -62,19 +62,21 @@ const todo = props => {
   //   }
   // }, [submittedTodo]);
   
-  const inputChangeHandler = event => {
-    // setTodoState({
-    //   userInput: event.target.value,
-    //   todoList: todoState.todoList
-    // });
-    setTodoName(event.target.value);
-  };
+  // const inputChangeHandler = event => {
+  //   // setTodoState({
+  //   //   userInput: event.target.value,
+  //   //   todoList: todoState.todoList
+  //   // });
+  //   setTodoName(event.target.value);
+  // };
   
   const todoAddHandler = () => {
     // setTodoState({
     //   userInput: todoState.userInput,
     //   todoList: todoState.todoList.concat(todoState.userInput)
     // });
+    const todoName = todoInputRef.current.value;
+    
     axios.post('https://test-326e3.firebaseio.com/todos.json', {
       name: todoName
     })
@@ -101,8 +103,7 @@ const todo = props => {
       <input
         type="text"
         placeholder="Todo"
-        onChange={inputChangeHandler}
-        value={todoName}
+        ref={todoInputRef}
       />
       <button type="button" onClick={todoAddHandler}>
         Add
